3f4e6b7278c8fc539a9963c502f864accd7000eb
YuliiaMatich <yuliia.matich@gmail.com>
2024-02-04 20:12:03 -0500

    basic inputs and datatypes

---
+++ app.py
@@ -0,0 +1,5 @@
+# basic inputs and datatypes
+name = input("Whats your name? ")
+birth_year = input("Enter your birth year: ")
+age = 2024 - int(birth_year)
+print("Hello " + name + " your age is " + str(age))
