3c23a6bc278dbe3a999121df893b074198d7a2df
YuliiaMatich <yuliia.matich@gmail.com>
2024-02-04 20:19:37 -0500

    basic sum calc

--- app.py
+++ app.py
@@ -3,3 +3,9 @@ name = input("Whats your name? ")
 birth_year = input("Enter your birth year: ")
 age = 2024 - int(birth_year)
 print("Hello " + name + " your age is " + str(age))
+
+# basic sum calc
+first_number = input("First: ")
+second_number = input("Second: ")
+Sum = float(first_number) + float(second_number)
+print("Sum: " + str(Sum));
