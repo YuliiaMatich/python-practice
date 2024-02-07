80c96b531291e1dc1b0267bd318ecc13c5720170
YuliiaMatich <yuliia.matich@gmail.com>
2024-02-06 22:37:35 -0500

    find str sequence

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-# basic inputs and datatypes
+# # basic inputs and datatypes
 name = input("Whats your name? ")
 birth_year = input("Enter your birth year: ")
 age = 2024 - int(birth_year)
 print("Hello " + name + " your age is " + str(age))
 
-# basic sum calc
+# # basic sum calc
 first_number = input("First: ")
 second_number = input("Second: ")
 Sum = float(first_number) + float(second_number)
 print("Sum: " + str(Sum));
+
+# strings finding index or -1
+course = "Python"
+print(course.find("on"))
