baa2dd6660a5b9e7694237d54f402ba775f96a16
YuliiaMatich <yuliia.matich@gmail.com>
2024-02-06 22:42:26 -0500

    str replace and find in

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-# # basic inputs and datatypes
+# basic inputs and datatypes
 name = input("Whats your name? ")
 birth_year = input("Enter your birth year: ")
 age = 2024 - int(birth_year)
 print("Hello " + name + " your age is " + str(age))
 
-# # basic sum calc
+# basic sum calc
 first_number = input("First: ")
 second_number = input("Second: ")
 Sum = float(first_number) + float(second_number)
 print("Sum: " + str(Sum));
 
-# strings finding index or -1
-course = "Python"
+# strings finding index or -1 and replace
+course = "Python for beginners"
 print(course.find("on"))
+print(course.replace("Py", "My"))
+print("for" in course)
