b882e732b9efd6b204986cf65758b6911def0d5b
YuliiaMatich <yuliia.matich@gmail.com>
2024-02-12 22:59:03 -0500

    list lookup and size

--- objects.py
+++ objects.py
@@ -2,3 +2,14 @@ numbers = [1,2,3,4,5]
 numbers.append(6)
 numbers.insert(0, -1)
 print(numbers)
+
+# removes item at idx 3
+numbers.remove(3)
+
+# numbers.clear() clears the list
+
+print(1 in numbers)
+print(10 in numbers)
+
+# size of list
+print(len(numbers))
