20bb85c9d0354d89c7d6e4b11c537913ce1702a3
YuliiaMatich <yuliia.matich@gmail.com>
2024-02-27 20:26:13 -0500

    find value and length

--- list_methods.py
+++ list_methods.py
@@ -1,8 +1,22 @@
 numbers = [1, 2, 3, 4, 5]
 ## to add value at the end
 numbers.append(6)
-print (numbers)
+print(numbers)
 
 ## to add value to a specific index
 numbers.insert(0, "s")
-print (numbers)
+print(numbers)
+
+## to remove specified value
+numbers.remove(3)
+print(numbers)
+
+## method to find a value in a list (true or false)
+print(6 in numbers)
+
+## to check the list length
+print(len(numbers))
+
+## to remove everything
+numbers.clear()
+print(numbers)
