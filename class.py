6d6ba26b14f861c2c3dd2752e2499ab8204f04db
YuliiaMatich <yuliia.matich@gmail.com>
2024-03-27 22:15:47 -0400

    class init

--- class.py
+++ class.py
@@ -1,10 +1,14 @@
 class Dog:
+  def __init__(self, name):
+    self.name = name
+    print(name)
+    
   def bark(self):
     print("bark")
 
   def add_one(self, x):
     return x + 1
-d = Dog() ## creating the instanse of class
+d = Dog("Tim") ## creating the instanse of class
 d.bark()
 print(type(d))
 print(d.add_one(5))
