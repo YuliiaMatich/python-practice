53707cf798f43b30f4a5b54c68b9f349865518dd
YuliiaMatich <yuliia.matich@gmail.com>
2024-03-27 22:27:19 -0400

    modifying attributes

--- class.py
+++ class.py
@@ -1,14 +1,25 @@
 class Dog:
-  def __init__(self, name):
+  def __init__(self, name, age): ## runs every time the instanse of class is created
     self.name = name
+    self.age = age
     print(name)
-    
+
   def bark(self):
     print("bark")
 
+  def get_name(self):
+    return self.name
+  
   def add_one(self, x):
     return x + 1
-d = Dog("Tim") ## creating the instanse of class
+  
+  def set_age(self, age):
+    self.age = age
+
+d = Dog("Tim", 12) ## creating the instanse of class
+d.set_age(23)
 d.bark()
 print(type(d))
 print(d.add_one(5))
+print(d.get_name())
+print(d.age)
