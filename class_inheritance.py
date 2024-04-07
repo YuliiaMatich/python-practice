aaa4ab931c93547dd6c7b5e377dbb47a1d34301b
YuliiaMatich <yuliia.matich@gmail.com>
2024-04-06 23:30:45 -0400

    class inheritance

---
+++ class_inheritance.py
@@ -0,0 +1,21 @@
+class Pet:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  
+  def show(self):
+    print(f"I am {self.name} and I am {self.age} years old")
+
+class Cat(Pet):
+  def speak(self):
+    print("Meow")
+
+class Dog(Cat):
+  def speak(self):
+    print("Bark")
+
+p = Pet("Tim", 19)
+p.show()
+c = Cat("Bill", 34)
+c.show()
+c.speak()
