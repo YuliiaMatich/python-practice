59303e21fd924da785065e29fc29bc706d564baf
YuliiaMatich <yuliia.matich@gmail.com>
2024-04-15 20:37:17 -0400

    class inheritance

--- class_inheritance.py
+++ class_inheritance.py
@@ -7,15 +7,21 @@ class Pet:
     print(f"I am {self.name} and I am {self.age} years old")
 
 class Cat(Pet):
+  def __init__(self, name, age, color):
+    super().__init__(name, age)
+    self.color = color
+
   def speak(self):
     print("Meow")
 
+  def show(self):
+    print(f"I am {self.name} and I am {self.age} years old and I am {self.color}")
 class Dog(Cat):
   def speak(self):
     print("Bark")
 
 p = Pet("Tim", 19)
 p.show()
-c = Cat("Bill", 34)
+c = Cat("Bill", 34, "blue")
 c.show()
 c.speak()
