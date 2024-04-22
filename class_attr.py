090f50756912a9be38772fe863028629f92d6dff
YuliiaMatich <yuliia.matich@gmail.com>
2024-04-21 21:50:55 -0400

    class method

--- class_attr.py
+++ class_attr.py
@@ -3,12 +3,19 @@ class Person:
 
   def __init__(self, name):
     self.name = name
-    Person.number_of_people += 1
+    Person.add_person()
+
+  @classmethod
+  def number_of_people_(cls):
+    return cls.number_of_people
+  
+  @classmethod
+  def add_person(cls):
+    cls.number_of_people += 1
 
 p1 = Person("tim")
-print(Person.number_of_people)
 p2 = Person("jill")
-print(Person.number_of_people)
 
-Person.number_of_people = 8
+
 print(p2.number_of_people)
+print(Person.number_of_people_())
