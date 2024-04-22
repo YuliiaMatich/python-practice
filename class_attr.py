f01d6d18be270c6a2baa213878a6af99cfb6d9c2
YuliiaMatich <yuliia.matich@gmail.com>
2024-04-21 21:36:55 -0400

    changing parametr of class on init

--- class_attr.py
+++ class_attr.py
@@ -3,9 +3,12 @@ class Person:
 
   def __init__(self, name):
     self.name = name
+    Person.number_of_people += 1
 
 p1 = Person("tim")
+print(Person.number_of_people)
 p2 = Person("jill")
+print(Person.number_of_people)
 
 Person.number_of_people = 8
 print(p2.number_of_people)
